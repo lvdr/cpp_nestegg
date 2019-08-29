ef4f827d9faecc95f4cfbf6a1669b6a63b075b15
VeeDeltaVee <45564258+VeeDeltaVee@users.noreply.github.com>
2019-08-29 17:57:05 +0530

    Implemented get immediate {word, byte}

--- Instructions.cpp
+++ Instructions.cpp
@@ -9,16 +9,47 @@ Instructions::Instructions()
     }
 
     // Add instructions here, by default instructions do nothing
+    instruction_array[0x69] = &execute_add_with_carry_immediate;
 }
 
 void Instructions::execute(uint8_t opcode, ComputerState &computer_state)
 {
+    increment_program_counter(computer_state);
+
     instruction_array[opcode](computer_state);
 }
 
 void Instructions::execute_nop(ComputerState &computer_state)
 {
-    uint16_t ip = computer_state.get_program_counter();
-    computer_state.set_program_counter(ip + 1);
     return;
+}
+
+
+void Instructions::execute_add_with_carry_immediate(ComputerState &computer_state)
+{
+           
+}
+
+
+uint8_t Instructions::get_immediate_byte(ComputerState &computer_state)
+{
+    uint8_t byte = computer_state.get_byte_from_memory(computer_state.get_program_counter());
+    increment_program_counter(computer_state);
+
+    return byte;
+}
+
+uint16_t Instructions::get_immediate_word(ComputerState &computer_state)
+{
+    uint16_t word = computer_state.get_word_from_memory(computer_state.get_program_counter());
+    increment_program_counter(computer_state);
+    increment_program_counter(computer_state);
+
+    return word;
+}
+
+
+void Instructions::increment_program_counter(ComputerState &computer_state)
+{
+    computer_state.set_program_counter(computer_state.get_program_counter() + 1);
 }
--- Instructions.hpp
+++ Instructions.hpp
@@ -11,11 +11,18 @@ public:
     Instructions();
 
     void execute(uint8_t opcode, ComputerState &computer_state);
+
     static void execute_nop(ComputerState &computer_state);
+    static void execute_add_with_carry_immediate(ComputerState &computer_state);
 
 private:
     typedef void (*InstructionFunction)(ComputerState&);
     std::array<InstructionFunction, 256> instruction_array;
+
+    uint8_t get_immediate_byte(ComputerState &computer_state);
+    uint16_t get_immediate_word(ComputerState &computer_state);
+
+    void increment_program_counter(ComputerState &computer_state);
 };
 
 #endif // header guard for INSTRUCTIONS_H
